982da930d46dbe43f8ab33e8dd4fef576673b7ed
gauravghodinde <gauravghodinde@gmail.com>
2024-08-21 04:41:34 +0530

    before refatoring

--- lib/components/repairRequestCard.dart
+++ lib/components/repairRequestCard.dart
@@ -9,81 +9,186 @@ class RepairRequestCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 5,
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            // Display the image
-            Image.network(
-              repairRequest.image,
-              width: double.infinity,
-              height: 200,
-              fit: BoxFit.cover,
-              loadingBuilder: (context, child, progress) {
-                if (progress == null) {
-                  return child;
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      value: progress.expectedTotalBytes != null
-                          ? progress.cumulativeBytesLoaded /
-                              (progress.expectedTotalBytes ?? 1)
-                          : null,
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.network(
+                repairRequest.image,
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, progress) {
+                  if (progress == null) {
+                    return child;
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: progress.expectedTotalBytes != null
+                            ? progress.cumulativeBytesLoaded /
+                                (progress.expectedTotalBytes ?? 1)
+                            : null,
+                      ),
+                    );
+                  }
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    height: 200,
+                    color: Colors.grey[200],
+                    child: Center(
+                      child: Icon(Icons.error, color: Colors.red[800]),
                     ),
                   );
-                }
-              },
-              errorBuilder: (context, error, stackTrace) {
-                return Center(
-                  child: Icon(Icons.error, color: Colors.red[800]),
-                );
-              },
+                },
+              ),
             ),
-            const SizedBox(height: 8),
-            // Display the description
+            const SizedBox(height: 16),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    decoration: BoxDecoration(
+                      color: _getStatusColor(repairRequest.status)
+                          .withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(
+                        color: _getStatusColor(repairRequest.status),
+                        width: 1.5,
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.info_outline,
+                          color: _getStatusColor(repairRequest.status),
+                          size: 20,
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          repairRequest.status.toUpperCase(),
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: _getStatusColor(repairRequest.status),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 12),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 141, 0, 0),
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(
+                          color: Colors.black38,
+                          width: 1.5,
+                        ),
+                      ),
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "CANCEL REQUEST",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
             Text(
               repairRequest.description,
               style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: Colors.black87,
               ),
             ),
-            const SizedBox(height: 8),
-            // Display the status
-            Text(
-              'Status: ${repairRequest.status}',
-              style: const TextStyle(
-                fontSize: 14,
-                color: Colors.grey,
-              ),
+            const SizedBox(height: 10),
+            Divider(color: Colors.grey[300]),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                const Icon(
+                  Icons.location_on,
+                  color: Colors.green,
+                  size: 18,
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Text(
+                    'Pickup Address: ${repairRequest.pickupAddress}',
+                    style: const TextStyle(
+                      fontSize: 15,
+                      color: Colors.black54,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 8),
-            // Display the pickup and delivery addresses
-            Text(
-              'Pickup Address: ${repairRequest.pickupAddress}',
-              style: const TextStyle(
-                fontSize: 14,
-                color: Colors.grey,
-              ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                const Icon(
+                  Icons.location_on,
+                  color: Colors.redAccent,
+                  size: 18,
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Text(
+                    'Delivery Address: ${repairRequest.deliveryAddress}',
+                    style: const TextStyle(
+                      fontSize: 15,
+                      color: Colors.black54,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'Delivery Address: ${repairRequest.deliveryAddress}',
-              style: const TextStyle(
-                fontSize: 14,
-                color: Colors.grey,
-              ),
-            ),
-            const SizedBox(height: 8),
-            // Display the date and time
-            Text(
-              'Request Date: ${_formatDate(repairRequest.dateTime)}',
-              style: const TextStyle(
-                fontSize: 14,
-                color: Colors.grey,
-              ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                const Icon(
+                  Icons.calendar_today,
+                  color: Colors.orange,
+                  size: 18,
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  'Request Date: ${_formatDate(repairRequest.dateTime)}',
+                  style: const TextStyle(
+                    fontSize: 15,
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
@@ -91,9 +196,23 @@ class RepairRequestCard extends StatelessWidget {
     );
   }
 
-  // Helper method to format date
   String _formatDate(String dateTime) {
     final DateTime parsedDate = DateTime.parse(dateTime);
-    return '${parsedDate.day}/${parsedDate.month}/${parsedDate.year} ${parsedDate.hour}:${parsedDate.minute}';
+    return '${parsedDate.day}/${parsedDate.month}/${parsedDate.year}  -  ${parsedDate.hour}hr:${parsedDate.minute}min';
+  }
+
+  Color _getStatusColor(String status) {
+    switch (status.toLowerCase()) {
+      case 'completed':
+        return Colors.green;
+      case 'in progress':
+        return Colors.blue;
+      case 'pending':
+        return Colors.orange;
+      case 'cancelled':
+        return Colors.red;
+      default:
+        return Colors.grey;
+    }
   }
 }
