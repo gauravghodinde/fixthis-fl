ef5f3234316bce480cb551fa736e9f5ecdbefdba
gauravghodinde <gauravghodinde@gmail.com>
2024-09-08 14:48:22 +0530

    changed the backend link

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
   // static String uri = "https://ft-final.vercel.app/";
-  static String uri = "http://172.70.101.255:3000/";
+  static String uri = "http://13.201.191.25:3000/";
   static int mainColorHsh = 0xff00BF63;
 }
